fa6a24db293db322ac9ed8d4451dd2b1380cd72b
fuookami <fuookami@163.com>
2020-10-11 10:11:04 +0800

    feature: 实现ex2.3。

--- Problem Solving With C++/cp2/ex-2_3/src/main.rs
+++ Problem Solving With C++/cp2/ex-2_3/src/main.rs
@@ -2,7 +2,6 @@ use std::io;
 
 const PROMOTION_PERCENT : f64 = 0.076;
 const MONTH_PER_YEAR : i64 = 12;
-const SUPPLYING_AGAIN_MONTHS : i64 = 6;
 
 fn read_annual_pay() -> f64 {
     let mut line = String::new();
@@ -15,6 +14,20 @@ fn read_annual_pay() -> f64 {
     match annual_pay {
         annual_pay if annual_pay >= 0. => annual_pay,
         _ => panic!("Not negative annual pay!")
+    }
+}
+
+fn read_month() -> i64 {
+    let mut line = String::new();
+    io::stdin().read_line(&mut line)
+        .expect("Failed to read line!");
+    let month : i64 = match line.trim().parse() {
+        Ok(num) => num,
+        Err(_) => panic!("Failed to parse to number!")
+    };
+    match month {
+        month if month >= 0 => month,
+        _ => panic!("Not negative month!")
     }
 }
 
@@ -31,9 +44,12 @@ fn main() {
         let annual_pay = read_annual_pay();
         let monthly_pay = annual_pay / MONTH_PER_YEAR as f64;
 
+        println!("Please supplying again months: ");
+        let supplying_again_months = read_month();
+
         let new_annual_pay = annual_pay * (1. + PROMOTION_PERCENT);
         let new_monthly_pay = new_annual_pay / MONTH_PER_YEAR as f64;
-        let supplying_again_pay = (new_monthly_pay - monthly_pay) * SUPPLYING_AGAIN_MONTHS as f64;
+        let supplying_again_pay = (new_monthly_pay - monthly_pay) * supplying_again_months as f64;
 
         println!("Supplying anain pay: {:.2}", supplying_again_pay);
         println!("New annual pay: {:.2}", new_annual_pay);
