14c2eb3556320a4160aef1fec6f7de9b7219f79d
fuookami <fuookami@163.com>
2020-11-25 19:48:27 +0800

    feature: impl ex3.9.

---
+++ Problem Solving With C++/cp3/ex-3_9/src/main.rs
@@ -0,0 +1,25 @@
+use rand::Rng;
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    
+    let mut count = 0;
+    for _ in 0..10000 {
+        let target = rng.gen_range(0, 3);
+        let select = rng.gen_range(0, 3);
+        if target == select {
+            count += 1;
+        }
+    }
+    println!("Bear the palm: {} / 10000", count);
+
+    count = 0;
+    for _ in 0..10000 {
+        let target = rng.gen_range(0, 3);
+        let select = rng.gen_range(0, 3);
+        if select != target {
+            count += 1;
+        }
+    }
+    println!("Bear the palm: {} / 10000", count);
+}
