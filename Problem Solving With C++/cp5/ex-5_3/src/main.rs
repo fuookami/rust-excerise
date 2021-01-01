5ac492b4c252c584afa6c9a279c36ca68953546c
fuookami <fuookami@163.com>
2021-01-01 21:55:44 +0800

    feature: impl ex5.3.

--- Problem Solving With C++/cp5/ex-5_3/src/main.rs
+++ Problem Solving With C++/cp5/ex-5_3/src/main.rs
@@ -3,45 +3,62 @@ use std::io;
 struct Time {
     hour: u32,
     minute: u32,
+    is_am: bool,
+}
+
+struct TimeDuration {
+    hour: u32,
+    minute: u32
 }
 
 impl Time {
-    fn new(minutes: u32) -> Time {
-        Time {
+    fn normalize(&self) -> u32 {
+        self.hour * 60 + self.minute + if !self.is_am { 12 * 60 } else { 0 }
+    }
+}
+
+impl TimeDuration {
+    fn new(minutes: u32) -> TimeDuration {
+        TimeDuration {
             hour: minutes / 60,
             minute: minutes % 60,
         }
-    }
-
-    fn normalize(&self) -> u32 {
-        self.hour * 60 + self.minute
     }
 }
 
 fn parse_time(time: &str) -> Result<Time, &'static str> {
     let mut hour = 0;
     let mut minute = 0;
+    let mut is_am = Option::<usize>::None {};
     let mut flag = 0;
 
-    for s in time.split(":") {
+    for s in time.split(|c| c == ':' || c == ' ') {
         match flag {
             0 => match s.trim().parse::<u32>() {
-                Ok(num) if num < 24 => hour = num,
+                Ok(num) if num < 12 => hour = num,
                 _ => return Result::Err("Invalid hour!"),
             },
             1 => match s.trim().parse::<u32>() {
                 Ok(num) if num < 60 => minute = num,
                 _ => return Result::Err("Invalid minute!"),
             },
+            2 => is_am = s.trim().find(|c: char| c.to_ascii_uppercase() == 'A'),
             _ => return Result::Err("Invalid time format!"),
         }
         flag = flag + 1;
     }
-
-    Result::Ok(Time {
-        hour: hour,
-        minute: minute,
-    })
+    match is_am {
+        Option::Some(value) if value == 0 => Result::Ok(Time {
+            hour: hour,
+            minute: minute,
+            is_am: true,
+        }),
+        _ => Result::Ok(Time {
+            hour: hour,
+            minute: minute,
+            is_am: false,
+        }),
+    }
 }
 
 fn read_line() -> String {
@@ -81,7 +98,7 @@ fn main() {
         };
 
         if begin_time < end_time {
-            let duration = Time::new(end_time - begin_time);
+            let duration = TimeDuration::new(end_time - begin_time);
             println!("Duration: {}:{}", duration.hour, duration.minute);
         } else {
             println!("Invalid duration!");
