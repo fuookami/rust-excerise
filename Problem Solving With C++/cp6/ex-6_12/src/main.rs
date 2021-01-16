cb000310ebd0d0d2bfa34f612fbcc586ea750d13
fuookami <fuookami@163.com>
2021-01-17 00:07:29 +0800

    feature: impl ex6.12.

---
+++ Problem Solving With C++/cp6/ex-6_12/src/main.rs
@@ -0,0 +1,43 @@
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::path::Path;
+
+fn read_file(path: &str) -> String {
+    let path = Path::new(path);
+    let display = path.display();
+
+    let mut fin = match File::open(&path) {
+        Err(why) => panic!("couldn't open {}: {}", display, why),
+        Ok(fin) => fin,
+    };
+
+    let mut s = String::new();
+    match fin.read_to_string(&mut s) {
+        Err(why) => panic!("couldn't read {}: {}", display, why),
+        Ok(_) => {}
+    };
+
+    return s;
+}
+
+fn check_words_amount(context: &str) -> usize {
+    context
+        .split(|c: char| c.is_whitespace() || c == ',' || c == '.' || c == ';' || c == ':' || c == '?')
+        .filter(|s| !s.is_empty())
+        .count()
+}
+
+fn read_file_name() -> String {
+    let mut line = String::new();
+    io::stdin()
+        .read_line(&mut line)
+        .expect("Failed to read line!");
+    line
+}
+
+fn main() {
+    println!("Enter file name: ");
+    let file_name = read_file_name();
+    println!("Words amount: {}", check_words_amount(&read_file(file_name.trim())));
+}
