52dc9bc37bde6c700066eec5ba20aa7a728f956b
fuookami <fuookami@163.com>
2021-01-17 20:04:31 +0800

    feature: impl ex7.4.

---
+++ Problem Solving With C++/cp7/ex-7_4/src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+
+fn calculate_sd(values: &Vec<f64>) -> f64 {
+    let average = values.iter().fold(0., |acc, x| acc + x) / values.len() as f64;
+    (values.iter().fold(0., |acc, x| acc + (x - average).powi(2)) / values.len() as f64).sqrt()
+}
+
+fn read_line() -> String {
+    let mut line = String::new();
+    io::stdin()
+        .read_line(&mut line)
+        .expect("Failed to read line!");
+    line
+}
+
+fn parse_values(context: &str) -> Vec<f64> {
+    let mut ret = Vec::<f64>::new();
+    for s in context.trim().split_ascii_whitespace().filter(|s| !s.is_empty()) {
+        match s.parse::<f64>() {
+            Result::Ok(value) => ret.push(value),
+            Result::Err(_) => {}
+        }
+    }
+    ret
+}
+
+fn main() {
+    println!("Enter values: ");
+    let values = parse_values(&read_line());
+    println!("SD: {}", calculate_sd(&values));
+}
