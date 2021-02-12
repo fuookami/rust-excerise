d71df9c7fe71a6f97ebb0fe16b38738ad79a3fbd
Sakurakouji Sakuya <bagawolf2014@yahoo.co.jp>
2021-02-12 23:31:13 +0800

    feature: impl ex7.6.

---
+++ Problem Solving With C++/cp7/ex-7_6/src/main.rs
@@ -0,0 +1,32 @@
+fn insert_sort_impl(arr: &mut Vec<i64>, len: usize) {
+    let mut i = 0;
+    while i < len {
+        if arr[i] > arr[len] {
+            break;
+        }
+        i += 1;
+    }
+    let temp = arr[len];
+    for j in ((i + 1)..(len + 1)).rev() {
+        arr[j] = arr[j - 1];
+    }
+    arr[i] = temp;
+}
+
+fn insert_sort(arr: &mut Vec<i64>, pred: fn(&Vec<i64>)) {
+    for i in 1..arr.len() {
+        insert_sort_impl(arr, i);
+        pred(arr);
+    }
+}
+
+fn print(arr: &Vec<i64>) {
+    for num in arr {
+        print!("{} ", num);
+    }
+    print!("\n");
+}
+
+fn main() {
+    insert_sort(&mut vec!(8, 6, 10, 2, 16, 4, 18, 14, 12, 10), print);
+}
