52b7c228263feab30c473bf6cdca9e0085b2c2ac
fuookami <fuookami@163.com>
2021-08-27 19:28:15 +0800

    feature: impl ex14.8.

---
+++ Problem Solving With C++/cp14/ex-14_8/src/main.rs
@@ -0,0 +1,26 @@
+fn generate_full_permutation_impl(vals: &Vec<u64>, curr_permutation: Vec<u64>, i: usize) {
+    if curr_permutation.len() == vals.len() {
+        for val in curr_permutation {
+            print!("{}, ", val);
+        }
+        print!("\n");
+    } else {
+        for j in 0..=i {
+            let mut this_permutation = curr_permutation.clone();
+            if j != i {
+                this_permutation.insert(j, vals[i]);
+            } else {
+                this_permutation.push(vals[i]);
+            }
+            generate_full_permutation_impl(vals, this_permutation, i + 1);
+        }
+    }
+}
+
+fn generate_full_permutation(vals: &Vec<u64>) {
+    generate_full_permutation_impl(vals, Vec::new(), 0);
+}
+
+fn main() {
+    generate_full_permutation(&vec![1, 2, 3, 4]);
+}
