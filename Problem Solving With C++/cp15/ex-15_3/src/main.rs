db20cc2796fca78c546cb537443495b11202bc49
fuookami <fuookami@163.com>
2021-12-04 17:10:19 +0800

    feature: impl ex15.3.

---
+++ Problem Solving With C++/cp15/ex-15_3/src/main.rs
@@ -0,0 +1,40 @@
+#[derive(Clone)]
+struct Person {
+    name: String,
+}
+
+trait Vehicle {
+    fn manufacturer(&self) -> &str;
+    fn engine_cylinder(&self) -> u64;
+    fn owner(&self) -> &Person;
+}
+
+trait Car: Vehicle {}
+
+struct SportCar {
+    _manufacturer: String,
+    _engine_cylinder: u64,
+    _owner: Person,
+}
+
+impl Vehicle for SportCar {
+    fn manufacturer(&self) -> &str {
+        &self._manufacturer
+    }
+
+    fn engine_cylinder(&self) -> u64 {
+        self._engine_cylinder
+    }
+
+    fn owner(&self) -> &Person {
+        &self._owner
+    }
+}
+
+impl Car for SportCar {
+
+}
+
+fn main() {
+    println!("Hello, world!");
+}
