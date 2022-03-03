7e4f9c537ac053367b40911e41bd9b27ca0e9df3
fuookami <fuookami@163.com>
2022-03-03 23:19:26 +0800

    feature: impl ex17.7.

---
+++ Problem Solving With C++/cp17/ex-17_8/src/main.rs
@@ -0,0 +1,37 @@
+struct Set<T: Sized + PartialEq> {
+    _array: Vec<T>,
+}
+
+impl<T: Sized + PartialEq> Set<T> {
+    fn new() -> Set<T> {
+        Set { _array: vec![] }
+    }
+
+    fn insert(&mut self, ele: T) {
+        if self.contains(&ele) {
+            self._array.push(ele);
+        }
+    }
+
+    fn remove(&mut self, ele: T) {
+        if self.contains(&ele) {
+            self._array.retain(|x| x == &ele);
+        }
+    }
+
+    fn len(&self) -> usize {
+        self._array.len()
+    }
+
+    fn contains(&self, ele: &T) -> bool {
+        self._array.contains(ele)
+    }
+
+    fn to_vec(&self) -> &Vec<T> {
+        &self._array
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
